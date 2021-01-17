b4b23bb2927f512aa62aabd72acd51e26a19a01f
Marek Galganski <marek.galganski@boncard.pl>
2021-01-18 00:22:39 +0100

    added pagination to bookings

--- app/Http/Controllers/Cars/CarBookingController.php
+++ app/Http/Controllers/Cars/CarBookingController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers\Cars;
 
 use App\Models\User;
 use App\Models\CarBooking;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CarBookingRequest;
 use App\Http\Resources\UserBookingsResource;
 
 class CarBookingController extends Controller
 {
-    public function getUserBookings($id)
+    public function getUserBookings($id, CarBookingRequest $request)
     {
         User::findOrFail($id);
         $bookings = CarBooking::with('car')
             ->where('user_id', $id)->orderBy('created_at', 'desc')
-            ->get();
+            ->paginate($request->perPage);
 
         return UserBookingsResource::collection($bookings);
     }
